0f03bd9193dcf6f229887a49b1961939b6195f01
Princess of Evil <laz2727@gmail.com>
2024-02-03 17:17:42 +0500

    97

--- src/data/princess/CarrierUI.java
+++ src/data/princess/CarrierUI.java
@@ -30,15 +30,7 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
-import org.codehaus.janino.JavaSourceClassLoader;
-import org.codehaus.janino.util.resource.ResourceFinder;
-import com.fs.starfarer.loading.Objectsuper;
 import java.util.List;
-
-import java.lang.Class;
-import java.lang.ClassLoader;
-
-import com.fs.starfarer.util.IntervalTracker;
 
 import java.awt.Color;
 import org.lazywizard.lazylib.ui.FontException;
@@ -69,61 +61,12 @@ public class CarrierUI extends BaseEveryFrameCombatPlugin
 		GREEN = Global.getSettings().getColor("textFriendColor");
 		BLUE = Global.getSettings().getColor("textNeutralColor");
 		
-	//	Object test = Global.getSettings().getHullModSpec("shielded_holds");
-	//	if (loader == null)
 			{
 			// It doesn't matter if the code is too long - the basic compiler will spit equally long IL from shorter code. Maybe with less variable usage, but that's VM's job and, frankly, only happens once on game load.
 			try
 				{
 				LazyFont fontd = LazyFont.loadFont("graphics/fonts/victor14.fnt");
 				font = fontd.createText();
-				/*
-				Class clazz = test.getClass();
-				ClassLoader cloader = clazz.getClassLoader();
-			//ResourceFinder resloader = (ResourceFinder) (new Objectsuper());
-				MethodHandle resloaderctor = MethodHandles.lookup().findConstructor(Objectsuper.class, MethodType.methodType(void.class));
-				ResourceFinder resloader = (ResourceFinder) (resloaderctor.invoke());
-			
-			//	loader = new JavaSourceClassLoader(cloader, resloader, "UTF-8");
-				loader = (JavaSourceClassLoader) (MethodHandles.lookup().findConstructor(JavaSourceClassLoader.class, MethodType.methodType(void.class, ClassLoader.class, ResourceFinder.class, String.class)).invoke(cloader, resloader, "UTF-8"));
-			//	heresy = (Heresy)loader.loadClass("data.princess.Heresy").newInstance();*/
-			
-				Class field = Class.forName("java.lang.reflect.Field", true, Class.class.getClassLoader());
-				
-				MethodType type = MethodType.methodType(field, new Class[] { String.class });
-				
-			//	logger.log(Level.INFO, fieldGetter);
-				
-				
-				
-			//	if (ac == null)
-					{
-				//	logger.log(Level.INFO, "AC NOT FOUND");
-					ac = Class.forName("com.fs.starfarer.combat.entities.ship.A.ooOO");
-					}
-				/*
-				MethodHandle fieldsGetter = MethodHandles.lookup().findVirtual(Class.class, "getDeclaredFields", type);
-				Object[] fields = ac.getDeclaredFields();
-				
-				MethodType typeType = MethodType.methodType(field, new Class[] { String.class });
-				MethodHandle fieldTypeGetter = MethodHandles.lookup().findVirtual(field, "getType", typeType);
-				
-				for (int i = 0; i < fields.length; i++)
-					{
-					if (fields[i].getType().equals(IntervalTracker.class))
-						heresy = fields[i];
-					}
-				if (heresy == null)*/
-					{
-				//	logger.log(Level.INFO, "HERESY NOT FOUND");
-					MethodHandle fieldGetter = MethodHandles.lookup().findVirtual(Class.class, "getDeclaredField", type);
-					heresy = fieldGetter.invoke(ac, "if.String");
-					}
-				
-				logger.log(Level.INFO, MethodHandles.lookup().findVirtual(field, "setAccessible", MethodType.methodType(void.class, new Class[] { boolean.class })));
-				MethodHandles.lookup().findVirtual(field, "setAccessible", MethodType.methodType(void.class, new Class[] { boolean.class })).invoke(heresy, true);
-				
-				heresyLoader = MethodHandles.lookup().findVirtual(field, "get", MethodType.methodType(Object.class, new Class[] { Object.class }));
 				}
 			catch (Throwable ex)
 				{
@@ -131,9 +74,7 @@ public class CarrierUI extends BaseEveryFrameCombatPlugin
 				}
 			}
 		}
-	/*
-	@Override
-	public void renderInUICoords(ViewportAPI viewport)*/
+		
 	public void advance(float amount, List<InputEventAPI> events)
 		{
 		if (Global.getCombatEngine().getCombatUI() == null || Global.getCombatEngine().getCombatUI().isShowingCommandUI() || !Global.getCombatEngine().isUIShowingHUD())
@@ -157,11 +98,10 @@ public class CarrierUI extends BaseEveryFrameCombatPlugin
 			{
 			FighterLaunchBayAPI bay = (FighterLaunchBayAPI)bays.get(i);
 			
-			IntervalTracker val = begForInterval(bay);
-			if (val != null)
+			try
 				{
-				int perc = (int)((1f - val.getRemaining() / val.getIntervalDuration()) * 100);
-				
+				int perc = (int)((1f - bay.getTimeUntilNextReplacement() / bay.getCurrReplacementIntervalDuration()) * 100);
+			
 				Color borderCol = GREEN;
 				if (!ship.isAlive())
 					borderCol = BLUE;
@@ -203,26 +143,12 @@ public class CarrierUI extends BaseEveryFrameCombatPlugin
 				GL11.glDisable(GL11.GL_BLEND);
 				GL11.glPopMatrix();
 				GL11.glPopAttrib();
-				}
 			
-		//	logger.log(Level.INFO, begForInterval(bay));
-			}
-		}
-		
-	public IntervalTracker begForInterval(FighterLaunchBayAPI fighter)
-		{
-		try
-			{
-			if (ac.isInstance(fighter))
+				}
+			catch (Throwable ex)
 				{
-			//	MethodHandles.lookup().findVirtual(field, "setAccessible", MethodType.methodType(void.class, new Class[] { boolean.class })).invoke(heresy, true);
-				return (IntervalTracker) heresyLoader.invoke(heresy, fighter);
+			//	logger.log(Level.INFO, "aleeex", ex);
 				}
 			}
-		catch (Throwable up)
-			{
-			logger.log(Level.INFO, "wee woo", up);
-			}
-		return null;
 		}
 	}
